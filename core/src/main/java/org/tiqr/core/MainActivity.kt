501b491223b9f6337f865cbe32de104ca111e59a
Iulia STANA <iulia.stana@dawn.tech>
2026-04-09 18:00:13 +0200

    Fix startup crash: inflate after super.onCreate

--- core/src/main/java/org/tiqr/core/MainActivity.kt
+++ core/src/main/java/org/tiqr/core/MainActivity.kt
@@ -79,8 +79,8 @@ open class MainActivity : BaseActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
 
         installSplashScreen()
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         enableEdgeToEdge(window)
 
