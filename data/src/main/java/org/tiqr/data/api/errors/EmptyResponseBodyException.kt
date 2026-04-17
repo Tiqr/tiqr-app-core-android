c5432e3ee01afe4cd02741d649c30a21cc3c7478
Iulia STANA <iulia.stana@dawn.tech>
2026-04-17 16:13:36 +0200

    remove attempt to changes to API calls to retrofit.Response<T>

--- data/src/main/java/org/tiqr/data/api/errors/EmptyResponseBodyException.kt
+++
@@ -1,6 +0,0 @@
-package org.tiqr.data.api.errors
-
-class EmptyResponseBodyException(
-    forRequest: String = "",
-    cause: Throwable? = null,
-) : Throwable("Empty response body: $forRequest", cause)
